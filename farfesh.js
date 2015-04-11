43ab95613342bb4137312f19cb37f3d1c872aeeb
abdelrahman146 <abdelrahman146@outlook.com>
2015-04-11 13:43:12 +0400

    first try

--- farfesh.js
+++ farfesh.js
@@ -1,23 +1,64 @@
+Stories = new Mongo.Collection("Stories");
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter stars at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get("counter");
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+        'submit .new-story': function(event) {
+            var story = event.target.story-text.value;
+            var age = event.target.age.value;
+            var faceType = event.target.faceType.value;
+            var language = event.target.lang_type.value;
+            Stories.insert({
+                storyText: story,
+                votes: [{faceType: faceType}],
+                count_votes: votes.length,
+                count_faces: function () {
+                    var face1 = 0, face2 = 0, face3 = 0, face4 = 0;
+                    for (var i; i < votes.length; i++) {
+                        if (votes[i].faceType == "face1") {
+                            face1++;
+                        } else if (votes[i].faceType == "face2") {
+                            face2++;
+                        } else if (votes[i].faceType == "face3") {
+                            face3++;
+                        } else if (votes[i].faceType == "face4") {
+                            face4++;
+                        }
+                    }
+                },
+                create_date: new Date(),
+                ipAddress: function () {
+                    if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+                    else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                    xmlhttp.open("GET", "http://api.hostip.info/get_html.php", false);
+                    xmlhttp.send();
+
+                    hostipInfo = xmlhttp.responseText.split("\n");
+
+                    for (i = 0; hostipInfo.length >= i; i++) {
+                        ipAddress = hostipInfo[i].split(":");
+                        if (ipAddress[0] == "IP") return ipAddress[1];
+                    }
+                },
+                age: age,
+                language: language
+            });
+        },
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set("counter", Session.get("counter") + 1);
+        }
+    });
 }
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+    if (Meteor.isServer) {
+        Meteor.startup(function () {
+            // code to run on server at startup
+        });
+    }
