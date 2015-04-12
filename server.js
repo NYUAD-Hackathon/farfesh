5798c58cef079dc3499c396667d660f8d303cc8d
Geoffrey Litt <glitt@panoramaed.com>
2015-04-12 12:44:43 +0400

    hide age on tweets, and add arabic tweets

--- server.js
+++ server.js
@@ -8,32 +8,48 @@ if (Meteor.isServer) {
   SyncedCron.add({
     name: 'Update tweets',
     schedule: function(parser) {
-      return parser.text('every 1 minute');
+      return parser.text('every 30 seconds');
     },
     job: function() {
-      Meteor.http.get("http://cheermeapp.cloudapp.net/tweetDir/en.tweets", function (err, res) {
-        if(res.statusCode == 200) {
-          var tweets = res.content.split("\n");
-          for(var i = 0; i < tweets.length; i++) {
-            var tweet = tweets[i];
-            var existingTweet = Stories.findOne({storyText: tweet})
-            if(existingTweet == null) {
-              Stories.insert({
-                  storyText: tweet,
-                  language: "arabic",
-                  face1value:0,
-                  face2value:0,
-                  face3value:0,
-                  face4value:0,
-                  tweet: true,
-                  createdAt: new Date(),
-                  country: "UAE"
-              });
-              break;
+
+      // Links array must align to languages array.
+      // This is really hacky, but time is short :)
+
+      var links = [
+        "http://cheermeapp.cloudapp.net/tweetDir/en.tweets",
+        "http://cheermeapp.cloudapp.net/tweetDir/ar.tweets"
+      ];
+
+      var languages = [
+        "english",
+        "arabic"
+      ];
+
+      for (var i = 0; i < links.length; i++) {
+        Meteor.http.get(links[i], function (err, res) {
+          if(res.statusCode == 200) {
+            var tweets = res.content.split("\n");
+            for(var i = 0; i < tweets.length; i++) {
+              var tweet = tweets[i];
+              var existingTweet = Stories.findOne({storyText: tweet})
+              if(existingTweet == null) {
+                Stories.insert({
+                    storyText: tweet,
+                    language: languages[i],
+                    face1value:0,
+                    face2value:0,
+                    face3value:0,
+                    face4value:0,
+                    tweet: true,
+                    createdAt: new Date(),
+                    country: "UAE"
+                });
+                break;
+              }
             }
           }
-        }
-      });
+        });
+      }
     }
   });
 
