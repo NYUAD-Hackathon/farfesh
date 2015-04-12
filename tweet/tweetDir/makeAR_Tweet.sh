66ea2ca5c2c0a2cb14beba95313253bf6c3321cc
azure provisioned user <cheermeapp@cheerMeApp.cheerMeApp.a3.internal.cloudapp.net>
2015-04-12 08:35:31 +0000

    using Arabic sentiment

--- tweet/tweetDir/makeAR_Tweet.sh
+++ tweet/tweetDir/makeAR_Tweet.sh
@@ -8,8 +8,8 @@ f2=$(date  --date="1 days ago" +"%Y-%m-%d".ar.tweets)
 cat $f1 $f2 | tr '\n' ' ' | sed 's:NEW_LINE:\n:g' | sed -e  's:^ ::' -e 's:^RT @[^:]*\: ::' -e 's:^RT @[^:]* ::' -e 's:http.*::' | sort | uniq -c  \
 | sort -r -n | awk '{$1="";print $0}' | sed 's:^ ::'  > ar.tweets.$$
 perl cleanTXT.pl ar.tweets.$$ ar.tweets.$$.$$
-cat ar.tweets.$$.$$ | sed -e s':^ ::'  -e 's:\s+: :' | grep -v ^$ > ar.tweets
-
+cat ar.tweets.$$.$$ | sed -e s':^ ::'  -e 's:\s+: :' | grep -v ^$ |  ./sentistrength.perl > ar.tweets.$$.$$.$$
+mv ar.tweets.$$.$$.$$ ar.tweets
 rm -fr ar.tweets.$$ ar.tweets.$$.$$
 
 sleep 10
